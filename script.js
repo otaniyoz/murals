9e8973031047d58564da5c636b404df9f0621e87
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2024-08-17 02:03:19 +0500

    added pullable curtain for image description

--- script.js
+++ script.js
@@ -31,6 +31,12 @@ window.onload = () => {
     else if (event.key === 'ArrowRight' || event.key === 'ArrowLeft') nextImage(event);
   }
 
+  function scrollCurtain(event) {
+    const curtain = document.getElementById('curtain');
+    const height = curtainYOffset - event.clientY;
+    curtain.style.height = `${Math.max(parseFloat(window.getComputedStyle(curtain).height), 0.2*window.innerHeight) + height}px`;
+  }
+
   function initModal() {
     const modal = document.getElementById('modal');
     const image = document.createElement('img');  
@@ -39,6 +45,8 @@ window.onload = () => {
     const leftButton = document.getElementById('left');
     const closeButton = document.getElementById('close');
     const imageDescriptionContainer = modal.children[0];
+    imageDescription.id = 'curtain';
+    imageDescription.classList.add('image-description');
     imageDescriptionContainer.appendChild(image);
     imageDescriptionContainer.appendChild(imageDescription);
     modal.addEventListener('pointerdown', hideModal);
@@ -56,6 +64,14 @@ window.onload = () => {
     imageDescriptionContainer[0].setAttribute('data-src', image.id);
     imageDescriptionContainer[1].textContent = `‘${image.dataset['title']}’ by ${image.dataset['author']}. ${image.dataset['location']}, ${image.dataset['country']}`;
     document.children[0].style.overflowY = 'hidden';
+    imageDescriptionContainer[1].style.width = `${imageDescriptionContainer[0].width + 8}px`;
+    imageDescriptionContainer[1].addEventListener('pointerdown', (event) => {
+      curtainYOffset = event.clientY;
+      document.addEventListener('pointermove', scrollCurtain);
+      document.addEventListener('pointerup', () => {
+        document.removeEventListener('pointermove', scrollCurtain);
+      });
+    });
     modal.classList.remove('hidden');
   }
 
@@ -67,11 +83,11 @@ window.onload = () => {
     while (i < imageFileNames.length && !found) {
       i++;
       if (imageFileNames[i] === imageDescriptionContainer[0].dataset.src) {
-        if (event.type === 'pointerdown' || event.key === 'ArrowRight') {
+        if ((event.type === 'pointerdown' && event.target.getBoundingClientRect().x > window.innerWidth / 2) || event.key === 'ArrowRight') {
           i = (i + 1) % imageFileNames.length; 
           found = true;         
         }
-        else if (event.type === 'pointerdown' || event.key === 'ArrowLeft') {
+        else if ((event.type === 'pointerdown' && event.target.getBoundingClientRect().x < window.innerWidth / 2) || event.key === 'ArrowLeft') {
           i = i - 1;
           if (i < 0) i = imageFileNames.length - 1;
           found = true;
@@ -82,6 +98,7 @@ window.onload = () => {
     imageDescriptionContainer[0].src = `images/${newImage.id}.jpg`;
     imageDescriptionContainer[0].setAttribute('data-src', newImage.id);
     imageDescriptionContainer[1].textContent = `‘${newImage.dataset['title']}’ by ${newImage.dataset['author']}. ${newImage.dataset['location']}, ${newImage.dataset['country']}`;
+    imageDescriptionContainer[1].style.width = `${imageDescriptionContainer[0].width + 8}px`;
   }
 
   function buildGallery() {
@@ -139,11 +156,15 @@ window.onload = () => {
       prevVW = vw;
     }
     timeoutId = setTimeout(() => { fitTextToWidth(title); }, 100);
+
+    const imageDescriptionContainer = document.getElementById('modal').children[0].children;
+    imageDescriptionContainer[1].style.width = `${imageDescriptionContainer[0].width + 8}px`;;
   }
 
   let prevVW = 0;
   let columns = 1;
   let timeoutId = null;
+  let curtainYOffset = 0;
   const imageFileNames = [];
   const breakPoints = [[0,319], [320,767], [768,1023], [1024,10000]];
   const muralsContainer = document.getElementById('murals-container');

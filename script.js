8ecbf7781ea740d915d0931866ed6b493d5f29cf
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2024-09-01 11:25:44 +0500

    improved image loading, updated info, updated json structure, refactored code, extended filtering, and changed filename structure

--- script.js
+++ script.js
@@ -12,17 +12,31 @@ window.onload = () => {
 
   function fitTextToWidth(element) {
     let iter = 0;
+    const step = 0.1;
     const minSize = 16;
     const maxSize = 640;
+    const maxIter = 100;
     let textWidth = element.scrollWidth;
     const containerWidth = element.parentNode.clientWidth;
     let fontSize = parseFloat(window.getComputedStyle(element).fontSize);
-    while (textWidth !== containerWidth && iter < 100 && fontSize > minSize && fontSize < maxSize) {
+    while (textWidth !== containerWidth && iter < maxIter && fontSize > minSize && fontSize < maxSize) {
       element.style.fontSize = `${fontSize}px`;
       textWidth = element.scrollWidth;
-      fontSize += 0.1 * (containerWidth - textWidth);
+      fontSize += step * (containerWidth - textWidth);
       iter++;
     }
+  }
+
+  function handleKeyPress(event) {
+    if (event.key === 'Escape') hideModal(event);
+    else if (event.key === 'ArrowRight' || event.key === 'ArrowLeft') nextImage(event);
+  }
+  
+  function scrollCurtain(event) {
+    const height = curtainYOffset - event.clientY;
+    const curtain = document.getElementById('curtain');
+    const baseHeight = parseFloat(window.getComputedStyle(curtain).height);
+    curtain.style.height = `${Math.max(baseHeight, 0.2*window.innerHeight) + height}px`;
   }
 
   function hideModal(event) {
@@ -34,20 +48,9 @@ window.onload = () => {
     }
   }
 
-  function handleKeyPress(event) {
-    if (event.key === 'Escape') hideModal(event);
-    else if (event.key === 'ArrowRight' || event.key === 'ArrowLeft') nextImage(event);
-  }
-
-  function scrollCurtain(event) {
-    const curtain = document.getElementById('curtain');
-    const height = curtainYOffset - event.clientY;
-    curtain.style.height = `${Math.max(parseFloat(window.getComputedStyle(curtain).height), 0.2*window.innerHeight) + height}px`;
-  }
-
   function initModal() {
-    const modal = document.getElementById('modal');
     const image = document.createElement('img');  
+    const modal = document.getElementById('modal');
     const imageDescription = document.createElement('p');
     const rightButton = document.getElementById('right');
     const leftButton = document.getElementById('left');
@@ -64,30 +67,102 @@ window.onload = () => {
     document.addEventListener('keyup', handleKeyPress);
   }
 
-  function showModal(event) {
-    const image = event.target;
+  function getWindowDimensions() {
     const w = window.innerWidth;
     const h = window.innerHeight;
     const vmin = (w > h) ? h / 100 : w / 100;
     const vmax = (w > h) ? w / 100 : h / 100;
+    return {w: w, h: h, vmin: vmin, vmax: vmax};
+  }
+
+  function getModalImageDimensions(imageRatio) {
+    let w, h;
+    const dimensions = getWindowDimensions();
+    const paddedWindowWidth = (dimensions.w <= breakPoints[2][0]) ? dimensions.w - 4*dimensions.vmin : dimensions.w - 24*dimensions.vmax;
+    if (imageRatio < dimensions.h / paddedWindowWidth) {
+      w = paddedWindowWidth;
+      h = imageRatio * paddedWindowWidth;
+    }
+    else {
+      h = dimensions.h;
+      w = dimensions.h / imageRatio;
+    }
+    return {w: w, h: h};
+  }
+
+  function getColumnImageDimensions(imageRatio, columns) {
+    const dimensions = getWindowDimensions();
+    const w = (dimensions.w - 4*dimensions.vmin - (columns - 1)*2*dimensions.vmax) / columns;
+    const h = imageRatio * w;
+    return {w: w, h: h};
+  }
+
+  function getImageDescription(imageDataset) {
+    let description = '';
+    const authors = imageDataset.author.split(',').filter(s => s !== '');
+    const numAuthors = authors.length;
+    if (lang === 'en') {
+      if (imageDataset.title) description += `‘${imageDataset.title}’`; 
+      if (imageDataset.title && imageDataset.year !== '0') description += `, ${imageDataset.year}`;
+      if (imageDataset.author && description.length) description += ' by ';
+      else if (imageDataset.author && !description.length) description += ' By ';
+      if (numAuthors) {
+        if (numAuthors === 1) description += authors[0];
+        else if (numAuthors === 2) description += `${authors[0]} and ${authors[1]}`;
+        else if (numAuthors >= 3) {
+          let i;
+          for (i = 0; i < numAuthors - 1; i++) {
+            description += authors[i];
+          }
+          description += `, and ${authors[i + 1]}`;
+        }
+      }
+      if (description.length) description += '. ';
+      description += `${imageDataset.location}, ${imageDataset.city}, ${imageDataset.country}`;
+      return description;
+    }
+    else if (lang === 'ru') {
+      if (imageDataset.title) description += `«${imageDataset.title}»`; 
+      if (imageDataset.title && imageDataset.year !== '0') description += `, ${imageDataset.year}`;
+      if (description.length) description += '. ';
+      if (numAuthors) {
+        if (numAuthors === 1) description += authors[0];
+        else if (numAuthors === 2) description += `${authors[0]} и ${authors[1]}`;
+        else if (numAuthors >= 3) {
+          let i;
+          for (i = 0; i < numAuthors - 1; i++) {
+            description += authors[i];
+          }
+          description += `, и ${authors[i + 1]}`;
+        }
+        description += '. ';
+      }
+      description += `${imageDataset.location}, ${imageDataset.city}, ${imageDataset.country}`;
+      return description;
+    }
+  }
+
+  function handleLoadedImage(event) {
+    const placeholder = document.getElementById(`${event.target.id}-placeholder`);
+    if (placeholder) placeholder.remove();
+    event.target.classList.remove('loading');
+    event.target.classList.add('loaded');
+  }
+
+  function showModal(event) {
+    const image = event.target;
     const modal = document.getElementById('modal');
     const imageDescriptionContainer = modal.children[0].children;
-    const paddedWindowWidth = (w <= breakPoints[2][0]) ? w - 4*vmin : w - 24*vmax;
+    const dimensions = getModalImageDimensions(image.dataset.ratio);
+    imageDescriptionContainer[0].classList = 'loaded';
     imageDescriptionContainer[0].src = `images/${image.id}.jpg`;
     imageDescriptionContainer[0].setAttribute('data-src', image.id);
     imageDescriptionContainer[0].setAttribute('data-ratio', image.dataset.ratio);
-    if (image.dataset.ratio < h / paddedWindowWidth) {
-      imageDescriptionContainer[0].style.width = `${paddedWindowWidth}px`;
-      imageDescriptionContainer[0].style.height = `${image.dataset.ratio*paddedWindowWidth}px`;
-    }
-    else {
-      imageDescriptionContainer[0].style.height = `${h}px`;
-      imageDescriptionContainer[0].style.width = `${h/image.dataset.ratio}px`;
-    }
-    if (lang === 'en') imageDescriptionContainer[1].textContent = `‘${image.dataset.title}’${(image.dataset.year !== '0') ? ', ' + image.dataset.year : ''} by ${image.dataset.author}. ${image.dataset.location}, ${image.dataset.country}`;
-    else if (lang === 'ru') imageDescriptionContainer[1].textContent = `«${image.dataset.title}»${(image.dataset.year !== '0') ? ', ' + image.dataset.year : ''}. ${image.dataset.author}. ${image.dataset.location}, ${image.dataset.country}`;
+    imageDescriptionContainer[0].style.width = `${dimensions.w}px`;
+    imageDescriptionContainer[0].style.height = `${dimensions.h}px`;
+    imageDescriptionContainer[1].textContent = getImageDescription(image.dataset);
     document.children[0].style.overflowY = 'hidden';
-    imageDescriptionContainer[1].style.width = `${imageDescriptionContainer[0].width + 8}px`;
+    imageDescriptionContainer[1].style.width = `${dimensions.w + 8}px`;
     imageDescriptionContainer[1].addEventListener('pointerdown', (event) => {
       curtainYOffset = event.clientY;
       document.addEventListener('pointermove', scrollCurtain);
@@ -103,21 +178,18 @@ window.onload = () => {
   function nextImage(event) {
     let i = -1;
     let found = false;
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    const vmin = (w > h) ? h / 100 : w / 100;
-    const vmax = (w > h) ? w / 100 : h / 100;
+    const w = window.innerWidth / 2;
     const modal = document.getElementById('modal');
+    const eventX = event.target.getBoundingClientRect().x;
     const imageDescriptionContainer = modal.children[0].children;
-    const paddedWindowWidth = (w <= breakPoints[2][0]) ? w - 4*vmin : w - 24*vmax;
     while (i < imageFileNames.length && !found) {
       i++;
       if (imageFileNames[i] === imageDescriptionContainer[0].dataset.src) {
-        if ((event.type === 'pointerdown' && event.target.getBoundingClientRect().x > window.innerWidth / 2) || event.key === 'ArrowRight') {
+        if ((event.type === 'pointerdown' && eventX > w) || event.key === 'ArrowRight') {
           i = (i + 1) % imageFileNames.length; 
           found = true; 
         }
-        else if ((event.type === 'pointerdown' && event.target.getBoundingClientRect().x < window.innerWidth / 2) || event.key === 'ArrowLeft') {
+        else if ((event.type === 'pointerdown' && eventX < w) || event.key === 'ArrowLeft') {
           i = i - 1;
           if (i < 0) i = imageFileNames.length - 1;
           found = true;
@@ -125,34 +197,21 @@ window.onload = () => {
       }
     }
     const newImage = document.getElementById(imageFileNames[i]);
+    const dimensions = getModalImageDimensions(newImage.dataset.ratio);
     imageDescriptionContainer[0].src = `images/${newImage.id}.jpg`;
     imageDescriptionContainer[0].setAttribute('data-src', newImage.id);
     imageDescriptionContainer[0].setAttribute('data-ratio', newImage.dataset.ratio);
-    if (newImage.dataset.ratio < h / paddedWindowWidth) {
-      imageDescriptionContainer[0].style.width = `${paddedWindowWidth}px`;
-      imageDescriptionContainer[0].style.height = `${newImage.dataset.ratio*paddedWindowWidth}px`;
-    }
-    else {
-      imageDescriptionContainer[0].style.height = `${h}px`;
-      imageDescriptionContainer[0].style.width = `${h/newImage.dataset.ratio}px`;
-    }
-    if (lang === 'en') imageDescriptionContainer[1].textContent = `‘${newImage.dataset.title}’${(newImage.dataset.year !== '0') ? ', ' + newImage.dataset.year : ''} by ${newImage.dataset.author}. ${newImage.dataset.location}, ${newImage.dataset.country}`;
-    else if (lang === 'ru') imageDescriptionContainer[1].textContent = `«${newImage.dataset.title}»${(newImage.dataset.year !== '0') ? ', ' + newImage.dataset.year : ''}. ${newImage.dataset.author}. ${newImage.dataset.location}, ${newImage.dataset.country}`;
+    imageDescriptionContainer[0].style.width = `${dimensions.w}px`;
+    imageDescriptionContainer[0].style.height = `${dimensions.h}px`;
+    imageDescriptionContainer[1].textContent = getImageDescription(newImage.dataset);
     imageDescriptionContainer[1].style.width = `${imageDescriptionContainer[0].width + 8}px`;
-  }
-
-  function handleLoadedImage(event) {
-    event.target.classList.remove('loading');
   }
 
   function buildGallery() {
     let imageCount = 0;
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    const vmin = (w > h) ? h / 100 : w / 100;
-    const vmax = (w > h) ? w / 100 : h / 100;
+    const dimensions = getWindowDimensions();
     breakPoints.forEach((bp, i) => {
-      if (w >= bp[0] && w <= bp[1]) columns = i + 1;
+      if (dimensions.w >= bp[0] && dimensions.w <= bp[1]) columns = i + 1;
     });
     const children = muralsContainer.children;
     const navBar = document.getElementById('nav-bar');
@@ -173,32 +232,57 @@ window.onload = () => {
       for (let key in data) {
         if (data.hasOwnProperty(key)) {
           data[key].images.forEach((image, i) => {
+            let imageTags = [];
             const col = document.getElementById(`col-${imageCount % columns}`);
+            const picture = document.createElement('picture');
             const img = document.createElement('img');
             img.src = '#';
             img.alt = image.title;
             img.id = image.filename;
-            img.setAttribute('data-country', data[key].title);
             img.setAttribute('data-year', image.year);
+            img.setAttribute('data-city', image.city);
             img.setAttribute('data-title', image.title);
             img.setAttribute('data-ratio', image.ratio);
             img.setAttribute('data-author', image.author);
+            img.setAttribute('data-country', data[key].title);
             img.setAttribute('data-location', image.location);
-            img.setAttribute('data-category', image.category);
+            imageTags.push(image.city);
+            imageTags.push(`${image.year}`);
+            imageTags.push(data[key].title);
             for (let category of image.category) {
+              imageTags.push(category);
               if (categories.indexOf(category) === -1) {
                 categories.push(category);
               }
             }
-            if (!checkedCategories.length || checkedCategories.every(v => image.category.includes(v))) {
-              imageFileNames.push(image.filename);
-              const imageWidth = (w - 4*vmin - (columns - 1)*2*vmax) / columns;
-              const imageHeight = parseFloat(image.ratio) * imageWidth; 
+            for (let author of image.author) {
+              imageTags.push(author);
+              if (categories.indexOf(author) === -1) {
+                categories.push(author);
+              }
+            }
+            if (categories.indexOf(data[key].title) === -1) categories.push(data[key].title);
+            if (categories.indexOf(`${image.year}`) === -1 && image.year !== 0) categories.push(`${image.year}`);
+            if (categories.indexOf(image.city) === -1) categories.push(image.city);
+
+            if (!checkedCategories.length || checkedCategories.every(v => imageTags.includes(v))) {
+              const imageDimensions = getColumnImageDimensions(image.ratio, columns);
+              const placeholder = document.createElement('div');
+              placeholder.id = `${img.id}-placeholder`;
+              placeholder.innerHTML = `<svg class="loading-roundbar" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.0 32.5C18.24 32.5 16.5 32.13 14.9 31.41C13.29 30.69 11.86 29.64 10.68 28.33L12.55 26.67C13.66 27.91 15.06 28.86 16.63 29.42C18.21 29.98 19.89 30.14 21.54 29.88C23.19 29.62 24.75 28.96 26.07 27.95C27.4 26.93 28.45 25.61 29.13 24.08C29.81 22.56 30.1 20.89 29.97 19.23C29.84 17.56 29.3 15.96 28.39 14.56C27.48 13.16 26.24 12.01 24.78 11.21C23.31 10.42 21.67 10.0 20.0 10.0V7.5C23.32 7.5 26.49 8.82 28.84 11.16C31.18 13.51 32.5 16.68 32.5 20.0C32.5 23.32 31.18 26.49 28.84 28.84C26.49 31.18 23.32 32.5 20.0 32.5Z" fill="var(--text-secondary)"/></svg>`;
+              placeholder.classList.add('image-placeholder');
+              placeholder.style.height = `${imageDimensions.h}px`;
+              placeholder.style.width = `${imageDimensions.w}px`;
+              picture.style.height = `${imageDimensions.h}px`;
+              picture.style.width = `${imageDimensions.w}px`;
+              img.style.height = `${imageDimensions.h}px`;
+              img.style.width = `${imageDimensions.w}px`;
               img.addEventListener('click', showModal);
               img.addEventListener('load', handleLoadedImage, { once: true });
-              col.appendChild(img);
-              img.style.width = `${imageWidth}px`;
-              img.style.height = `${imageHeight}px`;
+              imageFileNames.push(image.filename);
+              picture.appendChild(placeholder);
+              picture.appendChild(img);
+              col.appendChild(picture);
               imageCount++;
             }
           });
@@ -237,40 +321,31 @@ window.onload = () => {
   }
 
   function resizePage() {
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    const vmin = (w > h) ? h / 100 : w / 100;
-    const vmax = (w > h) ? w / 100 : h / 100;
-    const paddedWindowWidth = (w <= breakPoints[2][0]) ? w - 4*vmin : w - 24*vmax;
-    const sizeChanged = (w !== prevVW);
+   const dimensions = getWindowDimensions();
+    const paddedWindowWidth = (dimensions.w <= breakPoints[2][0]) ? dimensions.w - 4*dimensions.vmin : dimensions.w - 24*dimensions.vmax;
+    const sizeChanged = (dimensions.w !== prevVW);
     let breakPointCrossed = false;
     breakPoints.forEach((bp, i) => {
-      if (w >= bp[0] && w <= bp[1] && i + 1 !== columns) breakPointCrossed = true;
+      if (dimensions.w >= bp[0] && dimensions.w <= bp[1] && i + 1 !== columns) breakPointCrossed = true;
     });
     if (sizeChanged && breakPointCrossed) {
       buildGallery();
-      prevVW = w;
+      prevVW = dimensions.w;
     }
     else {
       const images = document.getElementsByTagName('img');
       for (let image of images) {
-        const imageWidth = (w - 4*vmin - (columns - 1)*2*vmax) / columns;
-        const imageHeight = parseFloat(image.dataset.ratio) * imageWidth; 
-        image.style.width = `${imageWidth}px`;
-        image.style.height = `${imageHeight}px`;  
+        const imageDimensions = getColumnImageDimensions(image.dataset.ratio, columns);
+        image.style.width = `${imageDimensions.w}px`;
+        image.style.height = `${imageDimensions.h}px`;  
       }
       debounce(fitTextToWidth(title), waitDuration);
     }
     const imageDescriptionContainer = document.getElementById('modal').children[0].children;
-    if (imageDescriptionContainer[0].dataset.ratio < h / paddedWindowWidth) {
-      imageDescriptionContainer[0].style.width = `${paddedWindowWidth}px`;
-      imageDescriptionContainer[0].style.height = `${imageDescriptionContainer[0].dataset.ratio*paddedWindowWidth}px`;
-    }
-    else {
-      imageDescriptionContainer[0].style.height = `${h}px`;
-      imageDescriptionContainer[0].style.width = `${paddedWindowWidth/imageDescriptionContainer[0].dataset.ratio}px`;
-    }
-    imageDescriptionContainer[1].style.width = `${imageDescriptionContainer[0].width + 8}px`;
+    const modalImageDimensions = getModalImageDimensions(imageDescriptionContainer[0].dataset.ratio);
+    imageDescriptionContainer[0].style.width = `${modalImageDimensions.w}px`;
+    imageDescriptionContainer[0].style.height = `${modalImageDimensions.h}px`;
+    imageDescriptionContainer[1].style.width = `${modalImageDimensions.w + 8}px`;
   }
 
   function changeLanguage() {
